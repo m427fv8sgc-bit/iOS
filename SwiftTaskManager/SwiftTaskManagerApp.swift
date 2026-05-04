7540ccf4bca2228be480288c5d2466152f40634a
Uttam Bhattacharjee <uttam.bhattacharjee.sp@dlh.de>
2026-05-04 23:43:17 +0530

    Initial Commit

---
+++ SwiftTaskManager/SwiftTaskManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTaskManagerApp.swift
+//  SwiftTaskManager
+//
+//  Created by Uttam Bhattcharjee on 04/05/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTaskManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftTaskManagerTests/SwiftTaskManagerTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTaskManagerTests.swift
+//  SwiftTaskManagerTests
+//
+//  Created by Uttam Bhattcharjee on 04/05/26.
+//
+
+import Testing
+@testable import SwiftTaskManager
+
+struct SwiftTaskManagerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
